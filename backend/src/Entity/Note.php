676f1714b018df8d215ecb3db8a14ca0212115d8
Matouš Wrožyna <mwrozyna77@icloud.com>
2025-07-21 04:33:11 +0200

    Add order and filter support

--- backend/src/Entity/Note.php
+++ backend/src/Entity/Note.php
@@ -2,6 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
@@ -23,6 +26,8 @@ use Symfony\Component\Serializer\Annotation\Groups;
         new Delete()
     ]
 )]
+#[ApiFilter(OrderFilter::class, properties: ['priority' => 'DESC'], arguments: ['orderParameterName' => 'order'])]
+#[ApiFilter(SearchFilter::class, properties: ['priority' => 'exact'])]
 #[ORM\Entity(repositoryClass: NoteRepository::class)]
 class Note
 {
