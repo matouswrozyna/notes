f3d0def27fbd5748f343ce5965a3f114e7cdfdb5
Matouš Wrožyna <mwrozyna77@icloud.com>
2025-07-21 13:23:31 +0200

    Simply skeleton by app-bar vuetify component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,28 @@
 <template>
-  <RouterView />
+  <v-app>
+    <v-app-bar :elevation="2">
+      <v-app-bar-title>Notes</v-app-bar-title>
+
+      <v-spacer />
+      <v-btn
+        color="primary"
+        @click="dialog = true"
+      >
+        <v-icon start>mdi-plus</v-icon>
+        Add note
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
 import { RouterView } from 'vue-router'
+
+const dialog = ref(false)
 </script>
